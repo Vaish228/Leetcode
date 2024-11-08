676c6efb5eceb572450be341943571fc72c20adf
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2024-11-08 15:49:03 +0530

    Time: 0 ms (100%), Space: 9.7 MB (32.31%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.c
@@ -0,0 +1,22 @@
+void sortColors(int* a, int ns) {
+    int zc=0,oc=0,tc=0;
+    for(int i=0;i<ns;i++)
+    {
+        if(a[i]==0)
+           zc++;
+        else if(a[i]==1)
+           oc++;     
+    }
+    for(int i=0;i<zc;i++)
+    {
+        a[i]=0;
+    }
+    for(int i=zc;i<zc+oc;i++)
+    {
+        a[i]=1;
+    }
+    for(int i=zc+oc;i<ns;i++)
+    {
+        a[i]=2;
+    }
+}
