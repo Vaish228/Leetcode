4e434074ef9d8fcba06dcf1e601f3c0f67066d39
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-01-02 19:30:54 +0530

    Time: 10 ms (8.73%), Space: 17.1 MB (51.83%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+       vector<string>v;
+        for(auto x:nums){
+            v.push_back(to_string(x));
+        }
+        sort(v.begin(),v.end(),[](string a,string b)->bool{
+            return a+b>b+a;
+        });
+        string ans="";
+        for(auto x:v){
+            ans+=x;
+        }
+        return ans[0]=='0' ? "0" : ans; 
+    }
+};
