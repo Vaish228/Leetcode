12e862fa2f2e0f094f24ba904a7d438dc0e2ca27
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-01-17 19:11:22 +0530

    Time: 0 ms (100%), Space: 41.7 MB (22.47%) - LeetHub

---
+++ 0886-score-of-parentheses/0886-score-of-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+         Stack<Integer> stack = new Stack<>();
+        stack.push(0); // Initialize with 0 to handle the outermost level
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(0); // Start a new group
+            } else {
+                int innerScore = stack.pop(); // Score of the innermost group
+                int outerScore = stack.pop(); // Score of the outer group
+                int currentScore = Math.max(2 * innerScore, 1); // Apply rules
+                stack.push(outerScore + currentScore); // Add to outer score
+            }
+        }
+
+        return stack.pop(); 
+    }
+}
