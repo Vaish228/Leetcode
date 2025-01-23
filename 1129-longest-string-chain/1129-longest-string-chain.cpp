c741a10c1b800d1482411febfa087c7bd8110a79
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-01-23 20:12:28 +0530

    Time: 90 ms (44.49%), Space: 56.5 MB (34.05%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,31 +1,49 @@
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
-         sort(words.begin(), words.end(), [](const string& a, const string& b) {
-        return a.size() < b.size();
-    });
+    //     sort(words.begin(), words.end(), [](const string& a, const string& b) {
+    //     return a.size() < b.size();
+    // });
+    // unordered_map<string, int> dp;
 
-    // Create a hashmap to store the longest chain ending at each word
+    // int maxLength = 0;
+
+    // for (const string& word : words) {
+    //     dp[word] = 1; 
+
+    //     for (int i = 0; i < word.size(); ++i) {
+    //         string predecessor = word.substr(0, i) + word.substr(i + 1);
+
+    //         if (dp.count(predecessor)) {
+    //             dp[word] = max(dp[word], dp[predecessor] + 1);
+    //         }
+    //     }
+
+    //     maxLength = max(maxLength, dp[word]);
+    // }
+
+    // return maxLength;
+    // }
+
+
     unordered_map<string, int> dp;
+    int ans=0;
+    sort(words.begin(), words.end(), sortByLength);
 
-    int maxLength = 0;
-
-    for (const string& word : words) {
-        dp[word] = 1; // Initialize with a chain of length 1
-
-        for (int i = 0; i < word.size(); ++i) {
-            // Create a predecessor by removing the ith character
-            string predecessor = word.substr(0, i) + word.substr(i + 1);
-
-            // If the predecessor exists in the hashmap, update the current chain length
-            if (dp.count(predecessor)) {
-                dp[word] = max(dp[word], dp[predecessor] + 1);
-            }
+    for(string wor : words)
+    {
+        for(int i=0;i<wor.length();i++)
+        {
+            string pre=wor.substr(0,i)+wor.substr(i+1);
+            dp[wor]=max(dp[wor],dp[pre]+1);
         }
-
-        maxLength = max(maxLength, dp[word]);
+        ans = max(ans,dp[wor]);
+    }
+    return ans;
     }
 
-    return maxLength;
+    static bool sortByLength(string &s1, string &s2){
+        return s1.length()<s2.length();
     }
+
 };
