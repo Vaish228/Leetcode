4285bebe05c33daa3ff10acb7a35db7429b04868
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-01-24 12:26:36 +0530

    Time: 0 ms (100%), Space: 8.5 MB (21.77%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.c
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.c
@@ -6,16 +6,15 @@
  * };
  */
 struct ListNode* middleNode(struct ListNode* head) {
-        if (head == NULL)
-        return NULL;
-
+    if(head==NULL)
+      return head;
     struct ListNode* slow = head;
     struct ListNode* fast = head;
-
-    while (fast != NULL && fast->next != NULL) {
+    while(fast!=NULL && fast->next!=NULL)
+    {
         slow = slow->next;
         fast = fast->next->next;
     }
-
     return slow;
+
 }
