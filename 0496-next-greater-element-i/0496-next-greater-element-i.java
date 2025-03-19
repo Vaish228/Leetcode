8d7a5291dd559a021e3ecbf803b67872f545fc4f
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-03-19 23:25:25 +0530

    Time: 2 ms (98.15%), Space: 43.6 MB (72.35%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> h = new HashMap<> ();
+        for(int i=0;i<nums2.length;i++)
+        {
+            h.put(nums2[i],i);
+        }
+        int[] res = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++)
+        {
+            res[i]=-1;
+            for(int j=h.get(nums1[i])+1; j<nums2.length; j++)
+            {
+                if(nums1[i]<nums2[j]){
+                    res[i]=nums2[j];
+                    break;
+                }
+            
+            }
+        }
+        return res;
+    }
+}
