152c70854097526b80d746de792a96439bac2a43
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-03-19 23:36:57 +0530

    Time: 2 ms (98.15%), Space: 43.9 MB (54.38%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -11,11 +11,11 @@ class Solution {
             res[i]=-1;
             for(int j=h.get(nums1[i])+1; j<nums2.length; j++)
             {
-                if(nums1[i]<nums2[j]){
+                if(nums1[i]<nums2[j])
+                {
                     res[i]=nums2[j];
                     break;
                 }
-            
             }
         }
         return res;
