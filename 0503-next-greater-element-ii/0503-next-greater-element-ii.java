dc1f985a81e097380e434435acfc2da94c424eb3
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-03-21 12:16:59 +0530

    Time: 90 ms (8.32%), Space: 46.2 MB (72.58%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,16 +1,30 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-        Stack<Integer> st = new Stack<>();
-        Arrays.fill(res, -1);
-        for (int i = 0; i < 2 * n; i++) {
-            int num = nums[i % n];
-            while (!st.isEmpty() && nums[st.peek()] < num) {
-                res[st.pop()] = num;
-            }
-            if (i < n) {
-                st.push(i);
+         int n = nums.length;
+         int[] res = new int[n];
+        // Stack<Integer> st = new Stack<>();
+        // Arrays.fill(res, -1);
+        // for (int i = 0; i < 2 * n; i++) {
+        //     int num = nums[i % n];
+        //     while (!st.isEmpty() && nums[st.peek()] < num) {
+        //         res[st.pop()] = num;
+        //     }
+        //     if (i < n) {
+        //         st.push(i);
+        //     }
+        // }
+        // return res;
+
+        for(int i=0;i<n;i++)
+        {
+            res[i]=-1;
+            for(int j=1;j<n;j++)
+            {
+                if(nums[(i+j)%n]>nums[i])
+                {
+                    res[i]=nums[(i+j)%n];
+                    break;
+                }
             }
         }
         return res;
