38bbe7084d8356de17822902a70cd328977e63dd
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-03-22 15:39:02 +0530

    Time: 0 ms (100%), Space: 42 MB (67.44%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,35 +1,42 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-     int left = 0;
-        int right = nums.length - 1;
+    //  int left = 0;
+    //     int right = nums.length - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+    //     while (left <= right) {
+    //         int mid = left + (right - left) / 2;
 
-            // Check if mid is the target
-            if (nums[mid] == target) {
-                return mid;
-            }
+    //         // Check if mid is the target
+    //         if (nums[mid] == target) {
+    //             return mid;
+    //         }
 
-            // Determine which side is sorted
-            if (nums[left] <= nums[mid]) {
-                // Left side is sorted
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;  // Target is in the left sorted part
-                } else {
-                    left = mid + 1;  // Target is in the right part
-                }
-            } else {
-                // Right side is sorted
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1;  // Target is in the right sorted part
-                } else {
-                    right = mid - 1;  // Target is in the left part
-                }
-            }
-        }
+    //         // Determine which side is sorted
+    //         if (nums[left] <= nums[mid]) {
+    //             // Left side is sorted
+    //             if (target >= nums[left] && target < nums[mid]) {
+    //                 right = mid - 1;  // Target is in the left sorted part
+    //             } else {
+    //                 left = mid + 1;  // Target is in the right part
+    //             }
+    //         } else {
+    //             // Right side is sorted
+    //             if (target > nums[mid] && target <= nums[right]) {
+    //                 left = mid + 1;  // Target is in the right sorted part
+    //             } else {
+    //                 right = mid - 1;  // Target is in the left part
+    //             }
+    //         }
+    //     }
 
-        return -1;
+    //     return -1;
+
+    for(int i=0;i<nums.length;i++)
+    {
+        if(nums[i]==target)
+            return i;
+    }
+    return -1;
 }
 }
