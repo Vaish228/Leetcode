3ece7b6fb60f0f89a1fe216548d34642ff13fdf5
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-03-22 16:52:40 +0530

    Time: 6 ms (51.85%), Space: 45.9 MB (80.63%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,14 +1,29 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rows=0, colms=matrix[0].length-1;
-        while(rows<matrix.length && colms>=0){
-            if(matrix[rows][colms]==target){
+        // int rows=0;
+        // int colms=matrix[0].length-1;
+        // while(rows<matrix.length && colms>=0){
+        //     if(matrix[rows][colms]==target){
+        //         return true;
+        //     }else if(target<matrix[rows][colms]){
+        //         colms--;
+        //     }else{
+        //         rows++;
+        //     }
+        // }
+        // return false;
+
+        int rowidx = matrix.length-1;
+        int colidx = 0;
+        int colbound = matrix[0].length;
+        while(rowidx>=0 && colidx<colbound)
+        {
+            if(target==matrix[rowidx][colidx])
                 return true;
-            }else if(target<matrix[rows][colms]){
-                colms--;
-            }else{
-                rows++;
-            }
+            else if(target>matrix[rowidx][colidx])
+                colidx++;
+            else
+                rowidx--;        
         }
         return false;
     }
