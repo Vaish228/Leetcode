5b61303b7585cfe6a3d449c37df3134be6ea7618
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-03-22 15:31:34 +0530

    Time: 2 ms (4.33%), Space: 46 MB (33.28%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,10 +1,28 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++)
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i]==target)
+        //         return i;
+        // }
+        // return -1;
+
+        Arrays.sort(nums);
+        int l=0;
+        int m;
+        int h=nums.length-1;
+        while(l<=h)
         {
-            if(nums[i]==target)
-                return i;
+            m=l+(h-l)/2;
+            if(nums[m]==target) 
+                return m;
+            else if(nums[m]<target)  
+                l=m+1;
+            else  
+                h=m-1;
         }
         return -1;
+
+        
     }
 }
