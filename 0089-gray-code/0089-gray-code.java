59262d9a4b82dc4b42345bfc9f44191be8be9bfe
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-04-11 11:47:58 +0530

    Time: 4 ms (98.94%), Space: 51.6 MB (47.46%) - LeetHub

--- 0089-gray-code/0089-gray-code.java
+++ 0089-gray-code/0089-gray-code.java
@@ -1,15 +1,21 @@
 class Solution {
     public List<Integer> grayCode(int n) {
         List<Integer> rs = new ArrayList<Integer>();
-        rs.add(0);
-        for(int i=0;i<n;i++)
-        {
-            int size = rs.size();
-            for(int j = size-1;j>=0;j--)
-            {
-                rs.add(rs.get(j)|1<<i);
-            }
+        // rs.add(0);
+        // for(int i=0;i<n;i++)
+        // {
+        //     int size = rs.size();
+        //     for(int j = size-1;j>=0;j--)
+        //     {
+        //         rs.add(rs.get(j)|1<<i);
+        //     }
+        // }
+        int totalNumbers = 1 << n; 
+
+        for (int i = 0; i < totalNumbers; i++) {
+            rs.add(i ^ (i >> 1)); 
         }
+
         return rs;
     }
 }
