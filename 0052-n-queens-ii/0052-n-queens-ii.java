64702b7859bf33bcb14e14c740277b931a124e65
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-06-12 13:24:31 +0530

    Time: 2 ms (61.23%), Space: 41.3 MB (23.4%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,21 +1,61 @@
 class Solution {
+     List<List<String>> res;
     public int totalNQueens(int n) {
-        return solve(0, 0, 0, 0, n);
+        char[][] board = new char[n][n];
+        res = new ArrayList<>();
+        for(int i =0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                board[i][j]='.';
+            }
+           // System.out.println();
+        } 
+        List<int[]> queen = new ArrayList<>();
+        dfs(board, 0 , queen);
+        return res.size();
+    }
+    public void dfs(char[][] board, int r, List<int[]> queen)
+    {
+        if(queen.size()==board.length)
+        {
+            List<String> rows = new ArrayList<>();
+            for(char[] row : board){
+                rows.add(new String(row));
+            }
+            res.add(rows);
+        }
+        for(int c =0 ;c<board.length;c++)
+        {
+            if(canAddQueen(r,c,board))
+            {
+                board[r][c]='Q';
+                queen.add(new int[]{r,c});
+                dfs(board, r+1, queen);
+                board[r][c]='.';
+                queen.remove(queen.size()-1);
+            }
+        }
+    }
+
+    public boolean canAddQueen(int r, int c, char board[][])
+    {
+         for (int i = 0; i < r; i++) {
+        if (board[i][c] == 'Q') return false;
+    }
+
+    // Check upper-left diagonal (↖)
+    for (int i = r - 1, j = c - 1; i >= 0 && j >= 0; i--, j--) {
+        if (board[i][j] == 'Q') return false;
+    }
+
+    // Check upper-right diagonal (↗)
+    for (int i = r - 1, j = c + 1; i >= 0 && j < board.length; i--, j++) {
+        if (board[i][j] == 'Q') return false;
+    }
+
+    return true;
     }
     
-    private int solve(int row, int cols, int diags, int antiDiags, int n) {
-        if (row == n)
-            return 1;
-
-        int availablePositions = ((1 << n) - 1) & ~(cols | diags | antiDiags);
-
-        int solutions = 0;
-        while (availablePositions != 0) {
-            int pos = availablePositions & -availablePositions;
-            availablePositions &= availablePositions - 1;
-            solutions += solve(row + 1, cols | pos, (diags | pos) << 1, (antiDiags | pos) >> 1, n);
-        }
-
-        return solutions;
-    }
+   
 }
