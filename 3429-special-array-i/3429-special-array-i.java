c02d95c496c505fd47956cfb38f4599cd3d2dc2b
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-06-26 20:09:42 +0530

    Time: 1 ms (33.46%), Space: 43.3 MB (74.44%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        int n=nums.length;
-	
-	for(int i=0; i<n-1; i++){
-		boolean flag = nums[i]%2==0;
-		boolean flag1 = nums[i+1]%2==0;
-		if((flag^flag1) == false) return false;
-	}
-
-	return true;
+        int n = nums.length;
+        for(int i=0;i<n-1;i++)
+        {
+            boolean f1 = nums[i]%2==0 ;
+            boolean f2 = nums[i+1]%2==0;
+            if((f1^f2) == false) 
+                return false;
+        }
+        return true;
     }
 }
