ec1217b305b88df9358038857342ec2e104fbb68
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-07-01 10:09:49 +0530

    Time: 97 ms (19.16%), Space: 57.8 MB (16.31%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,42 +1,72 @@
+// class Solution {
+//     public boolean f(int i, int target, int[] nums, int[][] dp)
+//     {
+//         if(target==0) return true;
+//         if(i==0) 
+//         //return target == nums[0];
+//         {
+//             if(nums[i]==target) return true;
+//             else return false;
+//         }
+//         if(dp[i][target]!=-1) return dp[i][target]==1 ? true : false;
+//         Boolean notpick = f(i-1, target, nums, dp);
+//         Boolean pick = false;
+//         if(nums[i]<=target)
+//         {
+//             pick = f(i-1, target-nums[i], nums, dp);
+//         }
+//         dp[i][target] = pick || notpick ? 1 : 0;
+//         return pick || notpick;
+
+//     }
+//     public boolean canPartition(int[] nums) {
+//        int sum =0;
+//        for(int a: nums){
+//         sum+=a;
+//        }
+//        if(sum%2==1) return false;
+//        int n = nums.length;
+//        int target = sum/2;
+//        int[][] dp = new int[n][target+1];
+//        Boolean ans = f(n-1, target, nums, dp);
+//        return ans;
+//     }
+// }
 class Solution {
-    boolean isSub(int i,int t,int[] a,int dp[][]){
-        if(t==0)
-        return true;
-        if(i==0){
-            if(a[i]==t)
-            return true;
-            else
-            return false;
+    public boolean f(int[] a, int i, int t, int[][] dp)
+    {
+        if(t==0) return true;
+        if(i==0)
+        {
+            if(a[i]==t) return true;
+            else return false;
         }
+        if(dp[i][t]!=-1) return dp[i][t]==1? true: false;
 
-        if(dp[i][t]!=-1)
-        return dp[i][t]==1?true:false;
+        Boolean notpick = f(a, i-1, t, dp);
+        Boolean pick = false;
+        if(t>=a[i])
+            pick = f(a, i-1, t-a[i], dp);
 
-        boolean notPick=isSub(i-1,t,a,dp);
-        boolean pick=false;
-        if(a[i]<=t)
-        pick=isSub(i-1,t-a[i],a,dp);
-
-        dp[i][t]=pick||notPick?1:0;
-        return pick||notPick;
+        dp[i][t] = pick||notpick ? 1:0;    
+        return pick||notpick;
     }
 
     public boolean canPartition(int[] nums) {
-        int sum=0;
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
+        int s = 0;
+        int n = nums.length;
+        for(int i=0;i<n;i++)
+        {
+            s+=nums[i];
         }
-        if(sum%2!=0)
-        return false;
-
-        int t=sum/2;
-        int dp[][]=new int[n][t+1];
-
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        if(s%2!=0) return false;
+        int t = s/2;
+        int[][] dp = new int[n][t+1];
+        for(int[] r : dp)
+        {
+            Arrays.fill(r, -1);
         }
-        return isSub(n-1,t,nums,dp);
+        return f(nums, n-1, t, dp);
     
     }
 }
