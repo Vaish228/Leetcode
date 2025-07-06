4f5e8d73a8e40843b79c2c4f0e9cc84778a6058d
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-07-06 15:37:56 +0530

    Time: 0 ms (100%), Space: 40.5 MB (50.74%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=1) return n;
+        int f = 1;
+        int s = 2;
+        for(int i=3;i<=n;i++)
+        {
+            int t = f+s;
+            f=s;
+            s=t;
+        }
+        return s;
+    }
+}
