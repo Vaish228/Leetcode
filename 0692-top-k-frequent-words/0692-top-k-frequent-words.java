d1229b3c843ebd73b8e0b95717e22644d31c98a2
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-07-10 06:13:17 +0530

    Time: 9 ms (14%), Space: 44.9 MB (55.73%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -6,12 +6,11 @@ class Solution {
             mp.put(s, mp.getOrDefault(s,0)+1);
         }
         List<Map.Entry<String, Integer>> en = new ArrayList<>(mp.entrySet());
-        en.sort((a,b)->b.getValue()-a.getValue());
+        en.sort((a,b)-> b.getValue() - a.getValue());
         for(int i=0;i<k;i++)
         {
             ls.add(en.get(i).getKey());
         }
-       // Collections.reverse(ls);
         return ls;
     }
 }
