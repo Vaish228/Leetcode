cea1bb3ccffc9d951927b814266225fb7be9aa7f
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-07-10 06:02:15 +0530

    Time: 8 ms (21.71%), Space: 44.8 MB (55.73%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> ls = new ArrayList<>();
+        TreeMap< String, Integer> mp = new TreeMap<>();
+        for(String s : words){
+            mp.put(s, mp.getOrDefault(s,0)+1);
+        }
+        List<Map.Entry<String, Integer>> en = new ArrayList<>(mp.entrySet());
+        en.sort((a,b)->b.getValue()-a.getValue());
+        for(int i=0;i<k;i++)
+        {
+            ls.add(en.get(i).getKey());
+        }
+       // Collections.reverse(ls);
+        return ls;
+    }
+}
