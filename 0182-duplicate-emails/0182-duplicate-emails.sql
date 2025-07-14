b0914c853a53e1a8c4aae6bd2120fc188abff571
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-07-14 15:29:06 +0530

    Time: 423 ms (17.28%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
--- select p1.email as Email from Person p1 join Person p2
--- on p1.id = p2.id
--- where p1.
+select distinct p1.email as Email from Person p1 join Person p2
+on p1.id <> p2.id and p1.email = p2.email;
 
-select email as Email from Person
-group by email
-having count(email)>1;
+-- select email as Email from Person
+-- group by email
+-- having count(email)>1;
