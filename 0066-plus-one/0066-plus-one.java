37a20e83952a9433f5294f5241ef5bf4f195e501
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-07-22 10:35:45 +0530

    Time: 3 ms (3.35%), Space: 42.1 MB (17.5%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -3,16 +3,15 @@ import java.math.BigInteger;
 class Solution {
     public int[] plusOne(int[] digits) {
         StringBuilder sb = new StringBuilder();
-        for(int digit : digits){
-            sb.append(digit);
+        for(int d : digits){
+            sb.append(d);
         }
-
-        BigInteger n = new BigInteger(sb.toString()).add(BigInteger.ONE);
-        String numStr = n.toString();
-        int[] result = new int[numStr.length()];
-        for(int i = 0; i < numStr.length(); i++) {
-            result[i] = numStr.charAt(i) - '0';
+        BigInteger nn = new BigInteger(sb.toString()).add(BigInteger.ONE);
+        String s = nn.toString();
+        int[] a = new int[s.length()];
+        for(int i=0;i<s.length();i++){
+            a[i] = s.charAt(i)-'0';
         }
-        return result;
+        return a;
     }
 }
