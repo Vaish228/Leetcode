6aa4a19e4c234295a2ab372781be6863de8f2a13
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-07-22 10:58:59 +0530

    Time: 1 ms (49.32%), Space: 41.2 MB (14.66%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,10 +1,19 @@
 class Solution {
     public boolean isUgly(int n) {
-        if(n<=0)  return false;
-        if(n==1) return true;
-        if(n%2==0) return isUgly(n/2);
-        else if(n%3==0) return isUgly(n/3);
-        else if(n%5==0) return isUgly(n/5);
-        return false;
+        // if(n<=0)  return false;
+        // if(n==1) return true;
+        // if(n%2==0) return isUgly(n/2);
+        // else if(n%3==0) return isUgly(n/3);
+        // else if(n%5==0) return isUgly(n/5);
+        // return false;
+
+        if(n<=0) return false;
+        int[] di = {2,3,5};
+        for(int i : di){
+            while(n%i ==0){
+                n/=i;
+            }
+        }
+        return n==1;
     }
 }
