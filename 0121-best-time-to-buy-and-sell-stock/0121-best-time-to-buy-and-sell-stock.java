00d28d2492eeae6a338e1d66bece33f13217ae97
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-07-22 11:31:01 +0530

    Time: 319 ms (1.93%), Space: 61.9 MB (41.05%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,7 +3,7 @@ class Solution {
         int min=Integer.MAX_VALUE;
         int max =0;
         for(int e : prices){
-            if(min>e)
+            if(e<min)
                 min = e;
             else
                 max = Math.max(max, e-min);    
