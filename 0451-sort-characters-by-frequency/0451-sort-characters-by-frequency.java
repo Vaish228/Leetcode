4cbfe44032126c4004979a30061bab6dee8b2b48
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-07-28 23:35:58 +0530

    Time: 19 ms (20.98%), Space: 45.4 MB (82.33%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,7 +1,25 @@
 class Solution {
-    public String frequencySort(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        sb.reverse();
-        return sb.toString();
+    public String frequencySort(String s)  {
+          int f[] = new int[128];
+          StringBuilder sb = new StringBuilder();
+          for(char ch : s.toCharArray()){
+            f[ch]++;
+          }
+
+          for(int i =0;i<s.length();i++){
+            int max = f[0];
+            int maxind = 0;
+            for(int j =1;j<128;j++){
+                if(max<f[j]){
+                    max = f[j];
+                    maxind = j;
+                }
+            }
+            for(int k =0;k<max;k++){
+                sb.append((char)maxind);
+            }
+            f[maxind]=0;
+          }
+          return sb.toString();
     }
 }
