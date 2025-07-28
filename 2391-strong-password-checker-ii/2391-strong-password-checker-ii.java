64e2928b8c3b8a91da9963f6c61219a5f725251d
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-07-28 23:57:10 +0530

    Time: 1 ms (84.28%), Space: 41.5 MB (79.09%) - LeetHub

--- 2391-strong-password-checker-ii/2391-strong-password-checker-ii.java
+++ 2391-strong-password-checker-ii/2391-strong-password-checker-ii.java
@@ -1,21 +1,22 @@
 class Solution {
     public boolean strongPasswordCheckerII(String password) {
          if(password.length()<8) return false;
-         boolean hasUpper = false;
-        boolean hasLower = false;
-        boolean hasDigit = false;
-        boolean hasSpecial = false;
+         boolean hu = false;
+         boolean hl = false;
+         boolean hd = false;
+         boolean hs = false;
 
-        for (int i = 0; i < password.length(); i++) {
+         for(int i=0;i<password.length();i++)
+         {
             char ch = password.charAt(i);
-            if (Character.isUpperCase(ch)) hasUpper = true;
-            else if (Character.isLowerCase(ch)) hasLower = true;
-            else if (Character.isDigit(ch)) hasDigit = true;
-            else if ("!@#$%^&*()-+".indexOf(ch) >= 0) hasSpecial = true;
+            if(Character.isUpperCase(ch))  hu = true;
+            else if(Character.isLowerCase(ch)) hl = true;
+            else if(Character.isDigit(ch)) hd = true;
+            else if("!@#$%^&*()-+".indexOf(ch)>=0)  hs = true;
 
-            if (i > 0 && ch == password.charAt(i - 1)) return false;
-        }
+            if(i>0 && ch==password.charAt(i-1)) return false; 
+         }
 
-        return hasUpper && hasLower && hasDigit && hasSpecial;
+         return hu && hl && hs && hd;
     }
 }
