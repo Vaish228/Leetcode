6e1633598111959fc42b2d1751ac32439e92f4e2
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-07-28 22:38:43 +0530

    Time: 17 ms (82.91%), Space: 60 MB (79.91%) - LeetHub

--- 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.java
+++ 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.java
@@ -1,28 +1,22 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
-        int n = garbage.length;
         
-        // Prefix sum of travel times
-        int[] prefixTravel = new int[n];
-        for (int i = 1; i < n; i++) {
-            prefixTravel[i] = prefixTravel[i - 1] + travel[i - 1];
+        int n = garbage.length;
+        int[] prev = new int[n];
+        for(int i=1;i<n;i++){
+            prev[i] = prev[i-1]+travel[i-1];
         }
 
-        // Total time: picking up all garbage
-        int totalTime = 0;
-        int lastM = 0, lastP = 0, lastG = 0;
-
-        for (int i = 0; i < n; i++) {
+        int tT=0;
+        int lG=0, lP=0, lM=0;
+        for(int i=0;i<n;i++){
             String g = garbage[i];
-            totalTime += g.length(); // 1 minute per garbage
-            if (g.contains("M")) lastM = i;
-            if (g.contains("P")) lastP = i;
-            if (g.contains("G")) lastG = i;
+            tT+=g.length();
+            if(g.contains("M")) lM=i;
+            if(g.contains("P")) lP=i;
+            if(g.contains("G")) lG=i;
         }
-
-        // Add travel times for each truck up to its last house
-        totalTime += prefixTravel[lastM] + prefixTravel[lastP] + prefixTravel[lastG];
-
-        return totalTime;
+        tT+=prev[lM]+prev[lG]+prev[lP];
+        return tT;
     }
 }
