1fc75caa5541554c60e0ee7b9b469ea6eaf4ebea
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-08-02 21:52:04 +0530

    Time: 3 ms (74.23%), Space: 46.3 MB (9.55%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int candy(int[] ra) {
+         int n = ra.length;
+
+         int[] ca = new int[n];
+         Arrays.fill(ca, 1);
+         for(int i=1; i<n;i++)
+         {
+            if(ra[i]>ra[i-1])
+            {
+                ca[i] = ca[i-1]+1;
+            }
+         }   
+         for(int i=n-2;i>=0;i--)
+         {
+            if(ra[i]>ra[i+1])
+            {
+                ca[i]=Math.max(ca[i],ca[i+1]+1);
+            }
+         }
+         int t=0;
+         for(int c : ca)
+         {
+            t+=c;
+         }
+         return t;
+
+    }
+}
