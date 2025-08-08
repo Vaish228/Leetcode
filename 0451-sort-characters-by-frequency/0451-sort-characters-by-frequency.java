b2344522f5e5a2523592d44c9a76277ba3260ec8
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-08-08 10:57:00 +0530

    Time: 12 ms (85.07%), Space: 45.4 MB (81.97%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,25 +1,41 @@
 class Solution {
     public String frequencySort(String s)  {
-          int f[] = new int[128];
-          StringBuilder sb = new StringBuilder();
-          for(char ch : s.toCharArray()){
-            f[ch]++;
-          }
+        //   int f[] = new int[128];
+        //   StringBuilder sb = new StringBuilder();
+        //   for(char ch : s.toCharArray()){
+        //     f[ch]++;
+        //   }
 
-          for(int i =0;i<s.length();i++){
-            int max = f[0];
-            int maxind = 0;
-            for(int j =1;j<128;j++){
-                if(max<f[j]){
-                    max = f[j];
-                    maxind = j;
-                }
+        //   for(int i =0;i<s.length();i++){
+        //     int max = f[0];
+        //     int maxind = 0;
+        //     for(int j =1;j<128;j++){
+        //         if(max<f[j]){
+        //             max = f[j];
+        //             maxind = j;
+        //         }
+        //     }
+        //     for(int k =0;k<max;k++){
+        //         sb.append((char)maxind);
+        //     }
+        //     f[maxind]=0;
+        //   }
+        //   return sb.toString();
+        Map<Character, Integer> mp = new HashMap<>();
+        for(char c : s.toCharArray()){
+            mp.put(c,mp.getOrDefault(c,0)+1);
+        }
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(mp.entrySet());
+        list.sort((a,b) -> b.getValue().compareTo(a.getValue()));
+        StringBuilder sb = new StringBuilder();
+        for(Map.Entry<Character, Integer> em : list){
+            char ch = em.getKey();
+            int count = em.getValue();
+            for(int i=0;i<count;i++){
+                sb.append(ch);
             }
-            for(int k =0;k<max;k++){
-                sb.append((char)maxind);
-            }
-            f[maxind]=0;
-          }
-          return sb.toString();
+        }
+        
+        return sb.toString();
     }
 }
