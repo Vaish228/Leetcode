f92945413f53a0393f8fc5e7bda6653a2f318d71
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-08-24 21:19:15 +0530

    Time: 17 ms (14.77%), Space: 49.4 MB (86.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,27 @@
 class Solution {
     public int majorityElement(int[] nums) {
-     int can = nums[0];
-        int c = 1;
-        for(int i=1;i<nums.length;i++)
-        {
-            if(nums[i]==can)
-                c++;
-            else
-                c--;
+    //  int can = nums[0];
+    //     int c = 1;
+    //     for(int i=1;i<nums.length;i++)
+    //     {
+    //         if(nums[i]==can)
+    //             c++;
+    //         else
+    //             c--;
 
-            if(c==0)
-            {
-                can=nums[i];
-                c=1;
-            }        
+    //         if(c==0)
+    //         {
+    //             can=nums[i];
+    //             c=1;
+    //         }        
+    //     }
+    //     return can;
+        Map<Integer, Integer> mp = new HashMap<>();
+        for(int i: nums){
+            mp.put(i, mp.getOrDefault(i,0)+1);
         }
-        return can;
+        List<Map.Entry<Integer, Integer>> en = new ArrayList<>(mp.entrySet());
+        en.sort((a,b)->b.getValue()-a.getValue());
+        return en.get(0).getKey();
     }
 }
