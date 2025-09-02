5a949cf0d322cbcf61d43b0d35ec4a4d019c442a
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-09-02 09:18:58 +0530

    Time: 9 ms (10.38%), Space: 45.5 MB (43.4%) - LeetHub

---
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        List<int []> res = new ArrayList<>();
+
+        int r =  rStart;
+        int c = cStart;
+
+        int tot = rows*cols;
+
+        int dir[][] = {{0,1} , {1,0} ,{0,-1} ,{-1,0}};
+        int d = 0;
+        int numSteps  =1;
+        while(res.size() < tot){
+           for(int i=0;i<2;i++){
+            for(int j=0;j<numSteps;j++){
+
+                if( 0<= r && r<rows && 0<=c && c<cols)
+                   res.add(new int[]{r,c});
+
+                if(res.size()== tot){
+                    return convert(res);
+                }
+
+                r += dir[d][0];
+                c+= dir[d][1];
+            }
+            d = (d+1)%4;
+           }
+           numSteps++;
+        }
+
+        return convert(res);
+    }
+
+    public static int[][] convert(List<int []> res){
+        int arr[][] = new int[res.size()][2];
+
+        for(int i=0;i<res.size();i++){
+            arr[i] = res.get(i);
+        }
+        return arr;        
+    }
+}
