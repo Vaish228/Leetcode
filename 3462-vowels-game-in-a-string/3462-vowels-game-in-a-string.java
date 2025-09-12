311f7b431f46dc33c3cbaa283607a83529b601fb
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-09-12 21:01:49 +0530

    Time: 4 ms (79.79%), Space: 45.4 MB (82.68%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -1,8 +1,14 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        for (int i = 0; i < s.length(); i++)
-            if ((0x104111 >> (s.charAt(i) - 97) & 1) != 0)
-                return true;
-        return false;        
+        // for (int i = 0; i < s.length(); i++)
+        //     if ((0x104111 >> (s.charAt(i) - 97) & 1) != 0)
+        //         return true;
+        // return false;        
+
+        for(char c : s.toCharArray()){
+            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+                return true;    
+        }
+        return false;
     }
 }
