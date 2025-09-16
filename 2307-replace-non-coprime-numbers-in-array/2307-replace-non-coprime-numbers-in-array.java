e9437c6226d4329912d3eb16684fce7a4309e12c
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-09-16 20:32:09 +0530

    Time: 35 ms (48.45%), Space: 57.9 MB (96.91%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.java
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.java
@@ -1,27 +1,24 @@
 class Solution {
-    public List<Integer> replaceNonCoprimes(int[] nums) {
-        List<Integer> stk = new ArrayList<>();
-        for (int x : nums) {
-            stk.add(x);
-            while (stk.size() > 1) {
-                int last = stk.get(stk.size() - 1);
-                int secondLast = stk.get(stk.size() - 2);
-                int g = gcd(last, secondLast);
-                if (g == 1) {
-                    break;
-                }
-                // merge
-                long lcm = (long) last * secondLast / g;
-                // remove last
-                stk.remove(stk.size() - 1);
-                // replace secondLast with lcm
-                stk.set(stk.size() - 1, (int) lcm);
-            }
-        }
-        return stk;
+    int gcd(int a,int b){
+       return b==0 ? a : gcd(b,a%b);
     }
-    
-    private int gcd(int a, int b) {
-        return (b == 0) ? a : gcd(b, a % b);
-    }        
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+      List<Integer> stk  = new ArrayList<>();
+      for(int num:nums){
+        stk.add(num);
+
+        while(stk.size()>1){
+            int num1=stk.get(stk.size()-1);
+            int num2=stk.get(stk.size()-2);
+            int g=gcd(num1,num2);
+            if(g==1) 
+               break;
+            int lcm= num1 / g * num2;
+            stk.remove(stk.size()-1);
+            stk.remove(stk.size()-1);
+            stk.add((int)lcm);
+        }
+      }  
+      return stk;   
+    }   
 }
