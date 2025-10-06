312fbd8f48aca3e8391fceebee171c5bc761ac8c
VAISHNAVI K <vaishnavik.22cse@kongu.edu>
2025-10-06 21:03:20 +0530

    Time: 10 ms (77.4%), Space: 44.9 MB (22.27%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.java
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -1,36 +1,30 @@
 class Solution {
-    int dirs[][] = {{0,-1},{0,1},{-1,0},{1,0}};
-    
     public int swimInWater(int[][] grid) {
-        int n = grid.length;
-        int ans = Integer.MAX_VALUE;
-        int low = 0, high = n * n - 1;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            boolean vis[][] = new boolean[n][n];
-            if (grid[0][0] <= mid && dfs(0, 0, mid, grid, vis)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        int n = grid.length;
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        boolean[][] visited = new boolean[n][n];
+        minHeap.add(new int[]{grid[0][0], 0, 0});
+        visited[0][0] = true;
+
+        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+
+        while (!minHeap.isEmpty()) {
+            int[] curr = minHeap.poll();
+            int elevation = curr[0], r = curr[1], c = curr[2];
+
+            if (r == n - 1 && c == n - 1) return elevation;
+
+            for (int[] d : dirs) {
+                int nr = r + d[0], nc = c + d[1];
+                if (nr >= 0 && nc >= 0 && nr < n && nc < n && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    minHeap.add(new int[]{Math.max(elevation, grid[nr][nc]), nr, nc});
+                }
             }
         }
-        return ans;
-    }
-
-    boolean dfs(int x, int y, int t, int[][] grid, boolean[][] vis) {
-        int n = grid.length;
-        vis[x][y] = true;
-        if (x == n - 1 && y == n - 1) return true;
-
-        for (int[] d : dirs) {
-            int nx = x + d[0], ny = y + d[1];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n &&
-                !vis[nx][ny] && grid[nx][ny] <= t) {
-                if (dfs(nx, ny, t, grid, vis)) return true;
-            }
-        }
-        return false;
+        return -1;
     }
 }
